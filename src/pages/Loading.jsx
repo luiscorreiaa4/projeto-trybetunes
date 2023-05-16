d65cc683263f800e1838347b60de62dc9d482be7
Luis Correia <pleluis47@gmail.com>
2023-05-16 17:28:33 -0300

    feat requisito 2

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+export default class Loading extends Component {
+  render() {
+    return (
+      <div>Carregando...</div>
+    );
+  }
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,63 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class Login extends Component {
+  state = {
+    name: '',
+    isLoading: false,
+  };
+
+  handleName = ({ target }) => {
+    this.setState({ name: target.value });
+  };
+
+  handleClick = async () => {
+    const { name } = this.state;
+    const { history } = this.props;
+    this.setState({
+      isLoading: true,
+    });
+    await createUser({
+      name,
+    });
+    this.setState({
+      isLoading: false,
+    });
+    history.push('./search');
+  };
+
   render() {
+    const { name, isLoading } = this.state;
+    const buttonDisabled = () => {
+      const MIN_LENGTH = 3;
+      return name.length < MIN_LENGTH;
+    };
+    if (isLoading) return <Loading />;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <input
+          type="text"
+          data-testid="login-name-input"
+          name="name"
+          value={ name }
+          onChange={ this.handleName }
+        />
+        <button
+          data-testid="login-submit-button"
+          disabled={ buttonDisabled() }
+          onClick={ this.handleClick }
+        >
+          ENTRAR
+        </button>
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
